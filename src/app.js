4a955dbbc65f8c555fb9bde7900b1c75125c75cd
Kunal Kumar <kunal34255@gmail.com>
2025-04-22 23:35:05 +0530

    feat: setup Express server with routes and error handler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-import express from 'express';
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
+import ErrorMiddleware from "./utils/error.js";
 const app = express();
 
+//plugins
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use("/api/v1/auth", authRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//unknown route
+app.use("/*splat", (req, res, next) => {
+  const err = new Error(`Route ${req.originalUrl} not found`);
+  err.statusCode = 404;
+  next(err);
+});
+app.use(ErrorMiddleware);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 
 app.listen(process.env.PORT, () => {
