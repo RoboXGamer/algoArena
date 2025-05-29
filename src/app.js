838f58650c9cb218b0cb24ffa8b84fdf15c4c053
Kunal Kumar <kunal34255@gmail.com>
2025-05-29 22:20:58 +0530

    fix: cors policy for production

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../", "public")));
 app.use(
   cors({
-    origin: "https://algoarenaa.netlify.app/",
+    origin: "https://algoarenaa.netlify.app",
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
